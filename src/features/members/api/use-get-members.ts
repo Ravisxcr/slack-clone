11434b3b3f6a8f13decc082d7f8655c6661bccae
Ravi <rranjankumarprajapati@gmail.com>
2026-06-27 20:57:49 +0530

    fix: worspace id

--- src/features/members/api/use-get-members.ts
+++ src/features/members/api/use-get-members.ts
@@ -6,11 +6,11 @@ import { api } from '@/../convex/_generated/api';
 import type { Id } from '@/../convex/_generated/dataModel';
 
 interface UseGetMembersProps {
-  workspaceId: Id<'workspaces'>;
+  workspaceId: Id<'workspaces'> | undefined;
 }
 
 export const useGetMembers = ({ workspaceId }: UseGetMembersProps) => {
-  const data = useQuery(api.members.get, { workspaceId });
+  const data = useQuery(api.members.get, workspaceId ? { workspaceId } : 'skip');
 
   const isLoading = data === undefined;
 
